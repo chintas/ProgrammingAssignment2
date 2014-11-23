de4ae18509ba94da99ef023e14a28d0a5079ce5d
Prasanth Chinta <prasanth.chinta@gmail.com>
2014-11-23 14:20:28 -0600

    With comments

--- cachematrix.R
+++ cachematrix.R
@@ -2,18 +2,23 @@
 ## functions do
 
 ## Write a short comment describing this function
-
+#This function creates a special "matrix" object that can cache its inverse
 makeCacheMatrix <- function(x = matrix()) {
-        m <- NULL
+        
+        #initialize temp matrix function s
+        s <- NULL
+        
+        #declare set method 
         set <- function(y) {
                 x <<- y
-                m <<- NULL
+                s <<- NULL #inialize superassignment
         }
         
+        #declare get method
         get <- function() x
         
-        setinverse <- function(solve) m <<- solve
-        getinverse <- function() m
+        setinverse <- function(solve) s <<- solve #returns matrix inverse
+        getinverse <- function() s #returns matrix inverse
         
         #It returns a list with named values where the values are the functions 
         #(aka closures) defined within the function. The fact that they're named 
@@ -27,16 +32,18 @@ makeCacheMatrix <- function(x = matrix()) {
 
 
 ## Write a short comment describing this function
-
+# This function computes the inverse 
+#of the special "matrix" returned by makeCacheMatrix 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
-        m <- x$getinverse()
-        if(!is.null(m)) {
+        
+        s <- x$getinverse()
+        if(!is.null(s)) { #return cached data
                 message("getting cached data")
-                return(m)
+                return(s)
         }
         data <- x$get()
-        m <- solve(data, ...)
-        x$setinverse(m)
-        m
+        s <- solve(data, ...) #use solve function to calculate matrix inverse.
+        x$setinverse(s)
+        s
 }
